1189d075791296f53413277a6ebb3f935358e451
Mirko Boehm (Endocode) <mirko@endocode.com>
2017-11-23 11:31:49 +0100

    Add verbose and version flag in POSIX style and loggers.

--- qmstr/qmstr-master/qmstr-master.go
+++ qmstr/qmstr-master/qmstr-master.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/spf13/pflag"
+)
+
+var (
+	// Info is the logger for INFO level output.
+	Info *log.Logger
+	// Log is the default logger.
+	Log *log.Logger
+)
 
 func main() {
-	fmt.Printf("I am the Quartermaster master!")
+	var verbose bool
+	var printVersion bool
+	pflag.BoolVarP(&verbose, "verbose", "v", false, "enable verbose log output")
+	pflag.BoolVarP(&printVersion, "version", "V", false, "print version and exit")
+	pflag.Parse()
+
+	var infoWriter io.Writer
+	if verbose {
+		infoWriter = os.Stdout
+	} else {
+		infoWriter = ioutil.Discard
+	}
+	Info = log.New(infoWriter, "INFO: ", log.Ldate|log.Ltime)
+	Log = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	Info.Printf("quartermaster master process starting")
+	defer Info.Printf("quartermaster master process exiting")
+	if printVersion {
+		fmt.Println("Quartermaster master 0.0.1")
+		return
+	}
 }
