6d255e4bf6963051883a264ac793d4d8816d9209
Markus Herpich <markus@endocode.com>
2017-12-05 10:12:32 +0100

    Make qmstr buildable in the vagrant box

--- qmstr-env.sh
+++ qmstr-env.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+rm -fr /vagrant/bin
 mkdir -p /vagrant/bin
-ln -s /vagrant/qmstr/qmstr-compile/qmstr-compile /vagrant/bin/gcc
+ln -s /home/vagrant/go/bin/qmstr-wrapper /vagrant/bin/gcc
 export PATH=/vagrant/bin:$PATH
 
 echo "Everything is ready!!"
--- vagrant-provisioner.sh
+++ vagrant-provisioner.sh
@@ -6,4 +6,11 @@ apt-get update
 # Go binaries will be installed in /usr/lib/go-1.9/bin:
 apt-get install -y golang-1.9-go autoconf
 
+echo 'PATH=/usr/lib/go-1.9/bin:$PATH' >> /home/vagrant/.profile
+
+mkdir -p /home/vagrant/go/{bin,pkg,src}
+mkdir -p /home/vagrant/go/src/qmstr-prototype
+ln -s /vagrant/qmstr /home/vagrant/go/src/qmstr-prototype/qmstr
+chown -R vagrant:vagrant /home/vagrant/go
+
 echo 'Run `. /vagrant/qmstr-env.sh` after compiling qmstr-compile'
