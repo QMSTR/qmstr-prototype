22d2833432b0502df05446880393c3674f70f1dc
Markus Herpich <markus@endocode.com>
2018-01-05 12:25:53 +0100

    Improve qmstr build in Docker
    Include test and more verbose output.

--- Docker/qmstr/helper-funcs.sh
+++ Docker/qmstr/helper-funcs.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function multiecho(){
+    str=$1
+    num=$2
+    v=$(printf "%-${num}s" "$str")
+    echo "${v// /${str}}"
+}
+
+function printheader(){
+    str=$1
+    echo
+    echo "$str"
+    multiecho "=" ${#str}
+}
+
 function quit(){
     curl http://localhost:9000/quit
 }
@@ -12,6 +26,10 @@ function printtargets(){
     curl http://localhost:9000/linkedtargets
 }
 
+function report(){
+    curl http://localhost:9000/report?id=$1
+}
+
 function build_cmake(){
     cd /build
     rm -fr build
@@ -22,7 +40,23 @@ function build_cmake(){
 }
 
 function build_qmstr(){
-    go get qmstr-prototype/qmstr/qmstr-master
-    go install qmstr-prototype/qmstr/qmstr-master
-    go install qmstr-prototype/qmstr/qmstr-wrapper
+    PROGS="qmstr-prototype/qmstr/qmstr-master qmstr-prototype/qmstr/qmstr-wrapper"
+    for p in $PROGS; do
+        printheader "Installing dependencies for $p"
+        go get -t $p
+        printheader "Building $p"
+        go build -v $p
+        printheader "Installing $p"
+        go install -v $p
+    done
+}
+
+function run_unittests() {
+    printheader "Running unit tests"
+    for p in $(ls /qmstr/qmstr); do
+        printheader "Testing package $p"
+        go get -t qmstr-prototype/qmstr/$p
+        go test -i qmstr-prototype/qmstr/$p
+        go test -v qmstr-prototype/qmstr/$p
+    done
 }
